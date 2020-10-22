ffee4972c79a69f50286bb8b8bae23ffb6d79f4a
Yunfeng <yzhao20@yahoo.com>
2020-10-22 22:53:18 +1100

    Update 4.2.3 Randomness.py

--- 4.2.3 Randomness.py
+++ 4.2.3 Randomness.py
@@ -1,13 +1,28 @@
 """
 1. Random module, and random, seed, randrange, randint functions.
-
 """
-from random import random    # The first random is a module, and the second one is a function that produces a Float number coming from [0.0, 1.0), NOT including right end point 1.0
-for i in range(5):  # Will produce five pseudorandom values
-    print(random())     # Random here is a function without argument, and their values are determined by the current (rather unpredictable) seed value, you can't simply guess them.
+from random import random  # Two randoms: one module and one function. The function produces one float number [0.0,1.0)
 
+for i in range(5):
+    print(random())  # Random function without argument.
 
-from random import random, seed   # Import the sedd function this time
-seed(50)   # Two cases here: seed() which sets the seed with the current time, seed(i) which sets the seed with the integer value i. In the latter case, we get the SAME values every time.
+from random import random, seed  # Import random and seed functions
+seed(50)  # The 50 argument is optional, if provided always get the same result. If not, it depends on current time.
 for i in range(5):
     print(random())
+
+from random import randrange, randint
+print(randrange(1), end=' ')  # randrange(end), end is not included.
+print(randrange(0, 1), end=' ')  # randrange(start, end), end is not included
+print(randrange(0, 1, 1), end=' ')  # randrange(start, end, step), end is not included.
+print(randint(0, 0)) # randint(left, right), BUT the right endpoint is included!
+"""
+2. choice, sample function to avoid repeatness.
+"""
+from random import choice, sample
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(choice(l)) # Return one(default) value from population
+print(sample(l, 5)) # Return a sample list (unordered) from population, and the # of elements shouldn't be greater than # of population.
+print(sample(l, 10))
+
+
