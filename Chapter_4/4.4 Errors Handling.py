307b9e84e9cc3440f61c9a1fe576104081c0a772
Yunfeng <yzhao20@yahoo.com>
2020-10-24 17:29:20 +1100

    Update 4.4 Errors Handling.py

--- Chapter_4/4.4 Errors Handling.py
+++ Chapter_4/4.4 Errors Handling.py
@@ -1,5 +1,14 @@
 """
 Python prefers to use "try... except" statement to handle errors rather than "if... else" statments.
 """
-
-try:
+try:  # This is the prefer approach.
+  x = int(input("> "))
+  y = 1 / x
+  print(y)
+except ValueError:  # ValueError is a builtins.
+  print("Please enter an integer value.")
+except ZeroDivisionError:   # ZeroDivisionError is a builtins.
+  print("Please enter an non-zero value")
+except:  # General exception besides the cases above.
+  print("Oh, dear.")
+print("The END.")  # This is outside the try-except statement.
