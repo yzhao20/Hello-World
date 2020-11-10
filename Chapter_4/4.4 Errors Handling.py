5b0924f92505306429a5fef5b0148531cfbe30dd
Yunfeng <yzhao20@yahoo.com>
2020-11-10 14:04:39 +1100

    Update 4.4 Errors Handling.py

--- Chapter_4/4.4 Errors Handling.py
+++ Chapter_4/4.4 Errors Handling.py
@@ -2,8 +2,8 @@
 Python prefers to use "try... except" statement to handle errors rather than "if... else" statments.
 """
 try:  # This is the prefer approach.
-  x = int(input("> "))
-  y = 1 / x
+  x = int(input("> "))   # Int conversion could get ValueError.
+  y = 1 / x    # Division could get an ZeroDivisionError.
   print(y)
 except ValueError:  # ValueError is a builtins.
   print("Please enter an integer value.")
